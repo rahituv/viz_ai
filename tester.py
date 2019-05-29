c67de0ea25c97db745bca49ed487bb98514c6d8d
Ronny Ahituv <ronnyahituv@skywatch-tech.com>
2019-05-29 09:56:59 +0300

    Adding multi-threading test

--- tester.py
+++ tester.py
@@ -1,4 +1,16 @@
 import requests
+import threading
+
+
+def run_for_success():
+    r = requests.post("http://localhost:8080/best_most_common_face", json={
+        "image_name_list": ["l1.jpg", "l2.jpg", "l3.jpg", "l4.jpg",
+                                                                     "l5.jpg", "l6.jpg", "l7.jpg"]
+    })
+
+    print r.status_code
+    print r.text
+
 
 r = requests.post("http://localhost:8080/best_most_common_face", json={
     "image_name_list": ["none_face.jpg"]
@@ -28,10 +40,10 @@ r = requests.post("http://localhost:8080/best_most_common_face", json={
 print r.status_code
 print r.text
 
-r = requests.post("http://localhost:8080/best_most_common_face", json={
-    "image_name_list": ["l1.jpg", "l2.jpg", "l3.jpg", "l4.jpg",
-                                                                 "l5.jpg", "l6.jpg", "l7.jpg"]
-})
 
-print r.status_code
-print r.text
+thread1 = threading.Thread(target=run_for_success)
+thread2 = threading.Thread(target=run_for_success)
+thread1.start()
+thread2.start()
+
+
